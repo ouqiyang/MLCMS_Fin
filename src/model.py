97a70f0959f02909e8157e5eec1e3f3291bc4737
Hanrui-Li <85330105+Hanrui-Li@users.noreply.github.com>
2025-01-29 23:16:52 +0100

    Updated file model.py

--- src/model.py
+++ src/model.py
@@ -2,12 +2,21 @@ import torch
 import torch.nn as nn
 
 class PedestrianSpeedNN(nn.Module):
-    def __init__(self, input_size, hidden_size=3):
+    def __init__(self, input_size, hidden_size=1024):
         super(PedestrianSpeedNN, self).__init__()
         self.model = nn.Sequential(
             nn.Linear(input_size, hidden_size),
             nn.ReLU(),
-            nn.Linear(hidden_size, 1)  # output speed
+            nn.Dropout(0.2),
+            nn.Linear(hidden_size, 512),
+            nn.ReLU(),
+            nn.Linear(512, 128),
+            nn.ReLU(),
+            nn.Linear(128, 32),
+            nn.ReLU(),
+            nn.Linear(32, 16),
+            nn.ReLU(),
+            nn.Linear(16, 1)  # output speed
         )
 
     def forward(self, x):
